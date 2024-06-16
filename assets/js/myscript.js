5bfefa50380372947af5fa9944d60a45d21ec826
yashborda7048 <yashborda123@gmail.com>
2024-06-16 17:10:04 +0530

    first commit

---
+++ assets/js/myscript.js
@@ -0,0 +1,28 @@
+//  light to dark change theme js
+
+// $(".change-theme").on("click", function () {
+//   if ($("body").hasClass("dark")) {
+//       $("body").removeClass("dark");
+//       $(".moon").addClass("d-none");
+//       $(".sun").removeClass("d-none");
+//       localStorage.setItem("theme", "light");
+//   } else {
+//       $("body").addClass("dark");
+//       $(".moon").removeClass("d-none");
+//       $(".sun").addClass("d-none");
+//       localStorage.setItem("theme", "dark");
+//   }
+// });
+
+// Check local storage for theme preference on page load
+// $(document).ready(function() {
+//   if (localStorage.getItem("theme") === "dark") {
+//       $("body").addClass("dark");
+//       $(".moon").removeClass("d-none");
+//       $(".sun").addClass("d-none");
+//   } else {
+//       $("body").removeClass("dark");
+//       $(".moon").addClass("d-none");
+//       $(".sun").removeClass("d-none");
+//   }
+// });
